108ce14da76d97ebe483232c70357f49774ef86d
jiyunmoves <jiyunmoves@gmail.com>
2014-02-25 21:20:46 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Letarts::Application.config.secret_key_base = 'a0ba4609172b7b534075dd4793254b8ce5ae4250b226c87049bc2334b6617eb08f336746787e94c1d26dbbc962805409db3c7bb5d23e584f8d993d3fde97463f'
